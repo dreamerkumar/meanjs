ea44b69fdf2b9c4cace89660f454234c3a3bd2ae
Vishal-Kumar <kumarfromatlanta@hotmail.com>
2016-05-14 14:31:34 -0400

    bower install should be before grunt build

--- build_source_files.sh
+++ build_source_files.sh
@@ -1,7 +1,7 @@
 echo "running build_and_run.sh"
 echo "Running npm install"
 npm install
-echo "Running grunt build"
-grunt build
 echo "Running bower install"
 bower install --allow-root
+echo "Running grunt build"
+grunt build
